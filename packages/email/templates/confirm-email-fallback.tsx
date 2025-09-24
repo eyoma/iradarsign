a952d5e9dc0ea2b0c49eeb676baaf332b2983f80
otobongfp <otobongfp@gmail.com>
2025-09-24 22:24:44 +0100

    chore: align logo with iradar

--- packages/email/templates/confirm-email-fallback.tsx
+++ packages/email/templates/confirm-email-fallback.tsx
@@ -21,15 +21,11 @@ export const ConfirmEmailFallbackTemplate = ({
         <Section>
           <Container className="mx-auto mb-2 mt-8 max-w-xl rounded-lg border border-solid border-slate-200 p-4 backdrop-blur-sm">
             <Section>
-              <Img
-                src={getAssetUrl('/static/logo.png')}
-                alt="Documenso Logo"
-                className="mb-4 h-6"
-              />
+              <Img src={getAssetUrl('/static/logo.png')} alt="iRadar Logo" className="mb-4 h-6" />
 
               <Section className="flex-row items-center justify-center">
                 <Text className="text-primary mx-auto mb-0 max-w-[80%] text-center text-lg font-semibold">
-                  Welcome to Documenso!
+                  Welcome to iRadar!
                 </Text>
 
                 <Text className="my-1 text-center text-base text-slate-400">
@@ -56,7 +52,7 @@ export const ConfirmEmailFallbackTemplate = ({
 
           <Container className="mx-auto max-w-xl">
             <Text className="text-center text-xs text-slate-400">
-              © 2024 Documenso. All rights reserved.
+              © 2024 iRadar. All rights reserved.
             </Text>
           </Container>
         </Section>
--- packages/email/templates/document-recipient-signed.tsx
+++ packages/email/templates/document-recipient-signed.tsx
@@ -42,11 +42,7 @@ export const DocumentRecipientSignedEmailTemplate = ({
               {branding.brandingEnabled && branding.brandingLogo ? (
                 <Img src={branding.brandingLogo} alt="Branding Logo" className="mb-4 h-6" />
               ) : (
-                <Img
-                  src={getAssetUrl('/static/logo.png')}
-                  alt="Documenso Logo"
-                  className="mb-4 h-6"
-                />
+                <Img src={getAssetUrl('/static/logo.png')} alt="iRadar Logo" className="mb-4 h-6" />
               )}
 
               <TemplateDocumentRecipientSigned
