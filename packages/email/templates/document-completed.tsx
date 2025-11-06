47355ffe151a6abbd31f3543fd957c15d9dcc60c
Ukeme Eyoma <ukeyoma@gmail.com>
2025-11-05 23:53:00 -0500

    fix: update link colors in document and template components to a new shade of green

--- packages/email/templates/document-completed.tsx
+++ packages/email/templates/document-completed.tsx
@@ -49,15 +49,6 @@ export const DocumentCompletedEmailTemplate = ({
         <Section className="bg-white">
           <Container className="mx-auto mb-2 mt-8 max-w-xl rounded-lg border border-solid border-slate-200 p-2 backdrop-blur-sm">
             <Section className="p-2">
-              {brandingData.brandingEnabled && brandingData.brandingLogo ? (
-                <Img src={brandingData.brandingLogo} alt="Branding Logo" className="mb-4 h-6" />
-              ) : (
-                <Img
-                  src={getAssetUrl('/static/logo.png')}
-                  alt="Documenso Logo"
-                  className="mb-4 h-6"
-                />
-              )}
 
               <TemplateDocumentCompleted
                 downloadLink={downloadLink}
--- packages/email/templates/document-created-from-direct-template.tsx
+++ packages/email/templates/document-created-from-direct-template.tsx
@@ -46,15 +46,6 @@ export const DocumentCreatedFromDirectTemplateEmailTemplate = ({
         <Section className="bg-white">
           <Container className="mx-auto mb-2 mt-8 max-w-xl rounded-lg border border-solid border-slate-200 p-2 backdrop-blur-sm">
             <Section className="p-2">
-              {branding.brandingEnabled && branding.brandingLogo ? (
-                <Img src={branding.brandingLogo} alt="Branding Logo" className="mb-4 h-6" />
-              ) : (
-                <Img
-                  src={getAssetUrl('/static/logo.png')}
-                  alt="Documenso Logo"
-                  className="mb-4 h-6"
-                />
-              )}
 
               <TemplateDocumentImage className="mt-6" assetBaseUrl={assetBaseUrl} />
 
