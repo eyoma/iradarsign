38ce6def90ad13442ad22bff6289e4dc4dcae92c
Ukeme Eyoma <ukeyoma@gmail.com>
2025-11-06 00:33:12 -0500

    fix: adjust logo sizes in app header and profile layout for improved visibility

--- apps/remix/app/utils/meta.ts
+++ apps/remix/app/utils/meta.ts
@@ -2,7 +2,7 @@ import { NEXT_PUBLIC_WEBAPP_URL } from '@documenso/lib/constants/app';
 
 export const appMetaTags = (title?: string) => {
   const description =
-    'Join iRadar, the open signing infrastructure, and get a 10x better signing experience. Pricing starts at $30/mo. forever! Sign in now and enjoy a faster, smarter, and more beautiful document signing process. Integrates with your favorite tools, customizable, and expandable. Support our mission and become a part of our open-source community.';
+    'iRadarSign,  signing infrastructure, and get a 10x better signing experience. Sign in now and enjoy a faster, smarter, and more beautiful document signing process.';
 
   return [
     {
@@ -15,7 +15,7 @@ export const appMetaTags = (title?: string) => {
     {
       name: 'keywords',
       content:
-        'iRadar, open source, DocuSign alternative, document signing, open signing infrastructure, open-source community, fast signing, beautiful signing, smart templates',
+        'iRadar, DocuSign alternative, document signing, fast signing, beautiful signing, smart templates',
     },
     {
       name: 'author',
@@ -27,7 +27,7 @@ export const appMetaTags = (title?: string) => {
     },
     {
       property: 'og:title',
-      content: 'iRadar - The Open Source DocuSign Alternative',
+      content: 'iRadarSign - The DocuSign Alternative',
     },
     {
       property: 'og:description',
@@ -47,7 +47,7 @@ export const appMetaTags = (title?: string) => {
     },
     {
       name: 'twitter:site',
-      content: '@documenso',
+      content: '@iradarsign',
     },
     {
       name: 'twitter:description',
